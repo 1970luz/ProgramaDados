054762d2c08d4588f077b5a1e2951293de96a40d
Luci <lucicaamall@gmail.com>
2023-09-06 21:08:25 -0500

    Agregar archivos de proyecto.

---
+++ ProgramaDados/FormCarasdeDados.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ProgramaDados
+{
+    partial class FormCarasdeDados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.btrReinicar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(79, 81);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(226, 61);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(79, 81);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(19, 168);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(315, 244);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Dado 1";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Dado 2";
+            this.columnHeader2.Width = 150;
+            // 
+            // btrReinicar
+            // 
+            this.btrReinicar.Location = new System.Drawing.Point(177, 7);
+            this.btrReinicar.Name = "btrReinicar";
+            this.btrReinicar.Size = new System.Drawing.Size(68, 51);
+            this.btrReinicar.TabIndex = 4;
+            this.btrReinicar.Text = "Salir";
+            this.btrReinicar.UseVisualStyleBackColor = true;
+            this.btrReinicar.Click += new System.EventHandler(this.btrReinicar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.btrReinicar);
+            this.groupBox1.Location = new System.Drawing.Point(31, 436);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(340, 78);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 51);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Tirar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Dado NUmero 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dado Numero 2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(340, 418);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // FormCarasdeDados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 526);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormCarasdeDados";
+            this.Text = "Dados";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private Button btrReinicar;
+        private GroupBox groupBox1;
+        private Label label1;
+        private Label label2;
+        private GroupBox groupBox2;
+        private Button button2;
+    }
+}
---
+++ ProgramaDados/FormCarasdeDados.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramaDados
+{
+    public partial class FormCarasdeDados : Form
+    {
+        public FormCarasdeDados()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btrReinicar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //se declaran las dos variables para cada dado
+
+            int dados1 = 0;
+            int dados2 = 0;
+            //se declara un metodo aleatorio
+            Random dadoaleatorio = new Random();
+
+            //se ejecuta   y obtienen los numeros aleatorios para cada dado
+            dados1 = dadoaleatorio.Next(1, 7);
+            dados2 = dadoaleatorio.Next(1, 7);
+
+            //para dado1
+            if (dados1 == 1)
+            {
+
+                pictureBox1.Image = Image.FromFile("cara1.jpg");
+            }
+            if (dados1 == 2)
+            {
+                pictureBox1.Image = Image.FromFile("cara2.jpg");
+            }
+            if (dados1 == 3)
+            {
+                pictureBox1.Image = Image.FromFile("cara3.jpg");
+            }
+            if (dados1 == 4)
+            {
+                pictureBox1.Image = Image.FromFile("cara4.jpg");
+            }
+            if (dados1 == 5)
+            {
+                pictureBox1.Image = Image.FromFile("cara5.jpg");
+            }
+            if (dados1 == 6)
+            {
+                pictureBox1.Image = Image.FromFile("cara6.jpg");
+            }
+
+          ;
+
+            //para dado2
+            if (dados2 == 1)
+            {
+
+                pictureBox2.Image = Image.FromFile("cara1.jpg");
+            }
+            if (dados2 == 2)
+            {
+                pictureBox2.Image = Image.FromFile("cara2.jpg");
+            }
+            if (dados2 == 3)
+            {
+                pictureBox2.Image = Image.FromFile("cara3.jpg");
+            }
+            if (dados2 == 4)
+            {
+                pictureBox2.Image = Image.FromFile("cara4.jpg");
+            }
+            if (dados2 == 5)
+            {
+                pictureBox2.Image = Image.FromFile("cara5.jpg");
+            }
+            if (dados2 == 6)
+            {
+                pictureBox2.Image = Image.FromFile("cara6.jpg");
+            }
+            //se agregan los numeros aleatorios generados en la tirada
+            ListViewItem item1 = new ListViewItem(dados1.ToString(), listView1.Items.Count - 1);
+            item1.SubItems.Add(dados2.ToString());
+            listView1.Items.Add(item1);
+
+        }
+    }
+}
